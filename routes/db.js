12a96fa948d5256749329df2fefbf109020428aa
Rawrunes <Rawrunes@rawrunes>
2020-12-01 22:23:12 +0100

    fixed save and load sketches

--- routes/db.js
+++ routes/db.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 const assert = require('assert');
 
 const url = 'mongodb://localhost:27017';
 const dbName = 'drawv';
 
-const client = new MongoClient(url);
 
 /* GET home page. */
 router.get('/', function(req, res, next) 
@@ -14,10 +13,10 @@ router.get('/', function(req, res, next)
     res.render('db', { title: 'db' });
 });
 
-router.post('/saveSketch', function(req, res, next) 
+router.post('/saveSketch', async function(req, res, next) 
 {
     //console.log(req.body);
-    client.connect( async (err, client) => 
+    /*client.connect( async (err, client) => 
     {
         assert.equal(null, err);
         console.log("Connected successfully");
@@ -35,12 +34,31 @@ router.post('/saveSketch', function(req, res, next)
             console.log(response);
         })
         //client.close();
-    })    
+    })    */
+    const client = new MongoClient(url,{ useUnifiedTopology: true });
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        console.log("Connected successfully");
+    
+        let r = await db.collection('sketches').insertOne({
+            drawstring : JSON.stringify(req.body),
+            timestamp : new Date()
+        });
+        assert.equal(1, r.insertedCount);
+        
+    }
+    catch(err){
+        console.log(err.stack);
+    }
+    finally{
+        await client.close();
+    }
 });
 
-router.get('/getSketch', function(req, res, next) 
+router.get('/getSketch', async function(req, res, next) 
 {
-    client.connect( async (err, client) => 
+    /*client.connect( async (err, client) => 
     {
         assert.equal(null, err);
         console.log("Connected successfully");
@@ -58,8 +76,30 @@ router.get('/getSketch', function(req, res, next)
             break;
         }
         //client.close();
-    })    
+    })    */
     //db.sketches.find().sort({timestamp:1})
+    const client = new MongoClient(url,{ useUnifiedTopology: true });
+    try {
+        console.log("test1");
+        await client.connect();
+        console.log("test2");
+        const db = client.db(dbName);
+        console.log("Connected successfully");
+        let cursor = await db.collection('sketches').find().sort({timestamp:-1});
+        if(await cursor.hasNext()){
+            let result = await cursor.next();
+            res.send(result);
+            await cursor.close();
+        }
+    }
+    catch(err){
+        console.log(err.stack);
+    }
+    finally{
+        console.log("test3");
+        await client.close();
+    }
+
 });
 
 router.get('/getGallery', function (req, res, next) 
